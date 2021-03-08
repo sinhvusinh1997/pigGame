54187b86b9a4efd4555dc7783ef151a5834f2711
Sinh Vu <sinhvusinh1997@gmail.comm>
2021-03-08 18:03:45 +0700

    add main js-first

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const DOMStrings = {
+  newBtn: document.querySelector('.btn-new'),
+  rollBtn: document.querySelector('.btn-roll'),
+  holdBtn: document.querySelector('.btn-hold'),
+  inputFinalScore: document.querySelector('.final-score'),
+}
